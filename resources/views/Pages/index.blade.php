88b4d87f9cb33e365a7506a96003254e7a77bda1
Lasantha <aalgperera@gmail.com>
2018-11-14 15:50:15 +1300

    new

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{config('app.name','JinnApp')}}</title>
+        <title>{{config('app.name','JinnAppNew')}}</title>
     
     </head>
     <body>
         <h1>...Juniper Distillery Home Page...</h1>
         <p>This is Juniper Distillery web portal.</p>
+        <p>New Changes<p>
         
     </body>
 </html>
