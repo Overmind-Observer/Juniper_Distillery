7f56d2a62ef70575c4788fa8986250e3ae8071d6
Lasantha <aalgperera@gmail.com>
2018-11-14 13:43:54 +1300

    Initial commit

---
+++ resources/views/Pages/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','JinnApp')}}</title>
+    
+    </head>
+    <body>
+        <h1>...Juniper Distillery Home Page...</h1>
+        <p>This is Juniper Distillery web portal.</p>
+        
+    </body>
+</html>
